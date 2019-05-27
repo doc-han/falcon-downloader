ea06751020a537d14dad53f12b0f09b4e853dbdb
Han <yahyafarhan48@gmail.com>
2019-05-27 13:28:21 +0000

    devTool off

--- index.js
+++ index.js
@@ -42,8 +42,6 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadFile('./lib/index.html');
-  // Open the DevTools.
-   mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
